911259366c906674f4021fcc53e94d496f325f8c
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-02-08 14:32:17 -0500

    Preparing for socket integration

--- src/views/dashboard/Solutions.vue
+++ src/views/dashboard/Solutions.vue
@@ -93,7 +93,7 @@
       <v-col
         col="12"
         sm="5"
-        v-for="(file, i) in fileobjects" 
+        v-for="(file, i) in files" 
         :key="i"
         >
         <base-material-card
@@ -119,7 +119,7 @@
                 fab
                 icon
                 x-small
-                @click="downloadfile(file.id)"
+                @click="downloadfile(file)"
               >
                 <v-icon
                   small
@@ -159,238 +159,33 @@
       return {
         selectedjobid: '',
         jobs : [],
-        fileobjects:[],
+        files:[],
         jobobjects: {},
-        colouredLine: {
-          data: {
-            labels: ["'06", "'07", "'08", "'09", "'10", "'11", "'12", "'13", "'14", "'15"],
-            series: [
-              [275, 500, 290, 55, 700, 700, 500, 750, 630, 900, 930],
-            ],
-          },
-          options: {
-            low: 0,
-            high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-            chartPadding: {
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-            },
-          },
-        },
-        dailySalesChart: {
-          data: {
-            labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-            series: [
-              [12, 17, 7, 17, 23, 18, 38],
-            ],
-          },
-          options: {
-            low: 0,
-            high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-            chartPadding: {
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-            },
-            showPoint: false,
-          },
-        },
-        dataCompletedTasksChart: {
-          data: {
-            labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
-            series: [
-              [230, 750, 450, 300, 280, 240, 200, 190],
-            ],
-          },
-          options: {
-            low: 0,
-            high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-            chartPadding: {
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-            },
-          },
-        },
-        emailsSubscriptionChart: {
-          data: {
-            labels: ['Ja', 'Fe', 'Ma', 'Ap', 'Mai', 'Ju', 'Jul', 'Au', 'Se', 'Oc', 'No', 'De'],
-            series: [
-              [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-
-            ],
-          },
-          options: {
-            lineSmooth: this.$chartist.Interpolation.none(),
-            axisX: {
-              showGrid: false,
-            },
-            low: 0,
-            high: 1000,
-            chartPadding: {
-              top: 0,
-              right: 5,
-              bottom: 0,
-              left: 0,
-            },
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function (value) {
-                  return value[0]
-                },
-              },
-            }],
-          ],
-        },
-        multipleBar: {
-          data: {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            series: [
-              [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-              [400, 200, 250, 575, 450, 300, 285, 370, 370, 410, 620, 690],
-            ],
-          },
-          options: {
-            seriesBarDistance: 10,
-            lineSmooth: this.$chartist.Interpolation.none(),
-            axisX: {
-              showGrid: false,
-            },
-            low: 0,
-            high: 900,
-            chartPadding: {
-              top: 0,
-              right: 5,
-              bottom: 0,
-              left: 0,
-            },
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function (value) {
-                  return value[0]
-                },
-              },
-            }],
-          ],
-        },
-        multipleLine: {
-          data: {
-            labels: ["'06", "'07", "'08", "'09", "'10", "'11", "'12", "'13", "'14", "'15"],
-            series: [
-              [275, 500, 290, 55, 700, 700, 500, 750, 630, 900, 930],
-              [575, 600, 490, 75, 300, 400, 700, 450, 130, 200, 330],
-              [575, 300, 890, 155, 640, 540, 800, 250, 230, 400, 630],
-            ],
-          },
-          options: {
-            low: 0,
-            high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-            chartPadding: {
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-            },
-          },
-        },
-        pie: {
-          data: {
-            series: [62, 32, 6],
-          },
-          options: {
-            labelInterpolationFnc: (value) => `${value}%`,
-          },
-        },
-        headers: [
-          {
-            sortable: false,
-            text: 'ID',
-            value: 'id',
-          },
-          {
-            sortable: false,
-            text: 'Name',
-            value: 'name',
-          },
-          {
-            sortable: false,
-            text: 'Salary',
-            value: 'salary',
-            align: 'right',
-          },
-          {
-            sortable: false,
-            text: 'Country',
-            value: 'country',
-            align: 'right',
-          },
-          {
-            sortable: false,
-            text: 'City',
-            value: 'city',
-            align: 'right',
-          },
-        ],
-        items: [
-          {
-            id: 1,
-            name: 'Dakota Rice',
-            country: 'Niger',
-            city: 'Oud-Tunrhout',
-            salary: '$35,738',
-          },
-          {
-            id: 2,
-            name: 'Minerva Hooper',
-            country: 'Curaçao',
-            city: 'Sinaai-Waas',
-            salary: '$23,738',
-          },
-          {
-            id: 3,
-            name: 'Sage Rodriguez',
-            country: 'Netherlands',
-            city: 'Overland Park',
-            salary: '$56,142',
-          },
-          {
-            id: 4,
-            name: 'Philip Chanley',
-            country: 'Korea, South',
-            city: 'Gloucester',
-            salary: '$38,735',
-          },
-          {
-            id: 5,
-            name: 'Doris Greene',
-            country: 'Malawi',
-            city: 'Feldkirchen in Kārnten',
-            salary: '$63,542',
-          },
-        ],
-        tabs: 0,
-        list: {
-          0: false,
-          1: false,
-          2: false,
-        },
       }
     },
     mounted: function(){
       this.getAllJobs()
     },
     methods: {
-      downloadfile(fid){
+      downloadfile(file){
+        var fileurl = new URL("/api/v1/downloadFile?id=" + file.id, document.baseURI);
+        console.log('downloading file: ',file.id);
+        // window.open(fileurl, '_blank');
+        let self = this
+        axios({
+          method: 'get',
+          url: fileurl,
+          responseType: 'arraybuffer'
+        })
+          .then(function(response) {
+            //response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+            const url = window.URL.createObjectURL(new Blob([response.data]))
+            const link = document.createElement('a')
+            link.href = url
+            link.setAttribute('download', file.name) //or any other extension
+            document.body.appendChild(link)
+            link.click()
+        })
 
       },
       openpreview(fid){
@@ -401,7 +196,7 @@
       },
       viewjobfiles(jid){
         this.selectedjobid = jid
-        this.fileobjects = []
+        this.files = []
         for (let fid of this.jobobjects[jid].files){
           axios.get('/api/v1/file', {
             params: {
@@ -410,7 +205,7 @@
           })
           .then((response) => {
             console.log(response.data)
-            this.fileobjects.push(response.data)
+            this.files.push(response.data)
           })
           .catch((error) => {
             console.log(error)
