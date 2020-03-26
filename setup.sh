0a67c97f0832f0397a064c40f6adbe2debdfcba7
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2020-03-25 23:51:01 -0400

    Took out the osx cxx

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@ git submodule update --recursive
 cd ./Fluigi-Cloud
 git fetch origin dev
 git checkout dev
-CXX=clang npm install
+npm install
 sudo chown -R $USER:$GROUP jobs/
 
 cd ..
 
 cd ./Neptune-UI
 git fetch origin dev
-git checkout dev
-CXX=clang npm install
-CXX=clang npm run build
+git checkout origin dev
+npm install
+npm run build
 
 cp -vr ./dist ../Fluigi-Cloud/dist
  
