9d52c657c909af915d8ebfcf7f869b2ec29f3a86
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2022-08-12 19:11:51 -0400

    Basic upload system is working fine now. Need to append the mongo model for the files objects now

--- job-runner/job_runner/filesystem.py
+++ job-runner/job_runner/filesystem.py
@@ -1,23 +1,25 @@
+import uuid
 import boto3
 from pprint import pprint
 from pathlib import Path
 
 from job_runner.setting import AWS_ENDPOINT_URL, AWS_S3_BUCKET_NAME
 
-
+S3_CLIENT = boto3.client(
+        "s3",
+        endpoint_url=AWS_ENDPOINT_URL
+    )
 
 def upload_file_using_client(file_location: Path):
     """
     Uploads file to S3 bucket using S3 client object
     :return: None
     """
-    s3 = boto3.client(
-        "s3",
-        endpoint_url=AWS_ENDPOINT_URL
-    )
     bucket_name = AWS_S3_BUCKET_NAME
-    object_name = file_location.name
-    file_path = file_location.absolute()
-
-    response = s3.upload_file(file_path, bucket_name, object_name)
-    pprint(response)  # prints None
+    file_name = file_location.name
+    file_path = str(file_location.absolute())
+    s3_object_name = f"{str(uuid.uuid4())}-{file_name}"
+    S3_CLIENT.upload_file(file_path, bucket_name, s3_object_name)
+    
+    print(f"Uploaded {file_path} to {bucket_name}/{file_name}")
+    
--- job-runner/job_runner/tasks.py
+++ job-runner/job_runner/tasks.py
@@ -1,10 +1,12 @@
 # import os
 from pathlib import Path
 import subprocess
+from job_runner.filesystem import upload_file_using_client
 
 from job_runner.server import celery_app
 import time
 from socket_io_emitter import Emitter
+import shutil
 
 
 @celery_app.task(name="add_task")
@@ -21,7 +23,7 @@ def test_execute(job_id:str):
     print("Executing task")
     
     # Create a new directory for the job and download all the input files to the directory
-    path = Path(f"/tmp/{job_id}")
+    path = Path(f"./jobs-tmp/{job_id}")
     output_path = path.joinpath("output")
     path.mkdir(parents=True, exist_ok=True)
     output_path.mkdir(parents=True, exist_ok=True)
@@ -29,11 +31,11 @@ def test_execute(job_id:str):
     # Start a subprocess to execute the test.sh script and pipe the output to a variable.
     io = Emitter({'host': 'localhost', 'port': 6379})
     with subprocess.Popen(
-        ["/bin/bash", "test.sh"], 
+        ["/bin/bash", "test.sh", str(output_path.absolute())], 
         # bufsize=1, 
         stdout=subprocess.PIPE, 
         stderr=subprocess.PIPE
-    ) as process, open('output.log', 'a+') as logfile:
+    ) as process, open(str(output_path.joinpath('output.log')), 'a+') as logfile:
     
         stdout_data = process.stdout.read() + process.stderr.read()
         logfile.write(stdout_data.decode("utf-8"))
@@ -43,7 +45,12 @@ def test_execute(job_id:str):
     
     # TODO: Upload the output files from the job directory to the s3 bucket
     
-    # TODO: Delete the job directory
+    # Loop through all the files in the output directory and upload them to the s3 bucket
+    for file in output_path.glob("*"):
+        upload_file_using_client(file.absolute())
+    
+    # Delete the job directory and all its contents
+    shutil.rmtree(str(path.absolute()))
     
     # Send the final Signal to the monitor that the job is complete
     io.Emit('EOP', "This is finished !")
