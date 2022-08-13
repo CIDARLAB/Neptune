5ecc0ea5740099b0519b768cfd1a62982503eddd
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2022-08-13 10:43:00 -0400

    Updated the task runner code to include provisions for passing the input files + s3 locations and the output files and s3 locations. Returned output will be a dictionary with the key value pairs of the filenames and their s3 objects. This way we can launch everything from node without the need for having any additional stuff.

--- job-runner/job_runner/filesystem.py
+++ job-runner/job_runner/filesystem.py
@@ -12,9 +12,13 @@ S3_CLIENT = boto3.client(
     )
 
 def upload_file_using_client(file_location: Path) -> str:
-    """
-    Uploads file to S3 bucket using S3 client object
-    :return: None
+    """ Uploads file to S3 bucket using S3 client object
+
+    Args:
+        file_location (Path): Location of the file to upload
+
+    Returns:
+        str: S3 object name  
     """
     bucket_name = AWS_S3_BUCKET_NAME
     file_name = file_location.name
@@ -25,4 +29,20 @@ def upload_file_using_client(file_location: Path) -> str:
     print(f"Uploaded {file_path} to {bucket_name}/{file_name}")
     
     return s3_object_name
+
+def download_file_using_client(s3_object_name: str, local_file_name: str, download_directory: Path) -> None:
+    """ Downloads file from S3 bucket using S3 client object
+
+    Args:
+        s3_object_name (str): name of the S3 object to download
+        local_file_name (str): name of the local file to download to
+        download_directory (Path): location of the file to download
+
+    Returns:
+        None: None
+    """
+    local_file_path_location = str(download_directory.joinpath(local_file_name).absolute())
+    S3_CLIENT.download_file(AWS_S3_BUCKET_NAME, s3_object_name, local_file_path_location)
     
+    print(f"Downloaded {s3_object_name} from {AWS_S3_BUCKET_NAME} to {local_file_path_location}")
+        
--- job-runner/job_runner/tasks.py
+++ job-runner/job_runner/tasks.py
@@ -1,8 +1,9 @@
 # import os
 from pathlib import Path
 import subprocess
-from typing import List
-from job_runner.filesystem import upload_file_using_client
+from sys import stdout
+from typing import Dict, List, Optional, Tuple
+from job_runner.filesystem import download_file_using_client, upload_file_using_client
 
 from job_runner.server import celery_app
 import time
@@ -22,7 +23,23 @@ def add(x, y):
     return x + y
 
 @celery_app.task(name="execute_task")
-def test_execute(job_id:str) -> List[str]:
+def execute(
+    job_id:str,
+    command: List[str],
+    input_file_s3_objects:List[Tuple[str,str]] = [], 
+    config_file_s3_object:Optional[Tuple[str, str]] = None,
+) -> Dict[str, str]:
+    """ Executes a command in a subprocess and returns a dictionary with the output file names and their corresponding s3 object names
+
+    Args:
+        job_id (str): Unique identifier for the job
+        command (List[str]): Command to execute in a subprocess
+        input_file_s3_objects (List[Tuple[str,str]]): List of tuples containing the input file name and its corresponding s3 object name
+        config_file_s3_object (Tuple[str, str]): Tuple containing the config file name and its corresponding s3 object name
+
+    Returns:
+        Dict[str, str]: Dictionary with the output file names and their corresponding s3 object names
+    """
     print("Executing task")
     
     # Create a new directory for the job and download all the input files to the directory
@@ -30,6 +47,14 @@ def test_execute(job_id:str) -> List[str]:
     output_path = path.joinpath("output")
     path.mkdir(parents=True, exist_ok=True)
     output_path.mkdir(parents=True, exist_ok=True)
+    
+    # Download all the input files to the directory
+    for (s3_object_name, local_file_name) in input_file_s3_objects:
+        download_file_using_client(s3_object_name, local_file_name, path)
+        
+    # Download the config file to the directory
+    if config_file_s3_object is not None:
+        download_file_using_client(config_file_s3_object[0], config_file_s3_object[1], path)
     
     # Start a subprocess to execute the test.sh script and pipe the output to a variable.
     io = Emitter({'host': SOCKETIO_REDIS_HOST, 'port': SOCKETIO_REDIS_PORT})
@@ -39,21 +64,26 @@ def test_execute(job_id:str) -> List[str]:
         stdout=subprocess.PIPE, 
         stderr=subprocess.PIPE
     ) as process, open(str(output_path.joinpath('output.log')), 'a+') as logfile:
-    
-        stdout_data = process.stdout.read() + process.stderr.read()
-        logfile.write(stdout_data.decode("utf-8"))
-        io.Emit('stdout', stdout_data.decode("utf-8"))
+        while process.poll() is None:
+            stdout_line = ""
+            stderr_line = ""
+            if process.stdout is not None:
+                stdout_line = process.stdout.readline().decode("utf-8")
+            if process.stderr is not None:
+                stderr_line = process.stderr.readline().decode("utf-8")
+            stdout_data = stdout_line + stderr_line
+            logfile.write(stdout_data)
+            io.Emit('stdout', stdout_data)
     
     logfile.close()
     
-    # TODO: Upload the output files from the job directory to the s3 bucket
-    
+    # Upload the output files from the job directory to the s3 bucket
     # Loop through all the files in the output directory and upload them to the s3 bucket
-    s3_object_names: List[str] = []
+    s3_object_names: Dict[str, str] = {}
     for file in output_path.glob("*"):
         s3_object_name = upload_file_using_client(file.absolute())
         
-        s3_object_names.append(s3_object_name)
+        s3_object_names[file.name]=s3_object_name
     
     # Delete the job directory and all its contents
     shutil.rmtree(str(path.absolute()))
